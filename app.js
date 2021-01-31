226f9803d3f89dcc87bde9da396a8aaca5e9b384
Marcelo Eduardo Alves Paixão Resende <marcelothebuilder@users.noreply.github.com>
2021-01-31 16:01:04 -0300

    app

---
+++ app.js
@@ -0,0 +1,27 @@
+(function () {
+    window.addEventListener("DOMContentLoaded", () => {
+        afterDomLoaded();
+    }, false);
+
+    function calculateAndShow() {
+        let val1 = document.querySelector("#date1").value;
+        let val2 = document.querySelector("#date2").value;
+
+        document.querySelector('#date1val').innerText = val1;
+        document.querySelector('#date2val').innerText = val2;
+
+       
+        let diff = Math.abs(new Date(val1).valueOf() - new Date(val2).valueOf());
+        document.querySelector('#result').innerText = `${diff}ms`;
+    }
+
+
+    function afterDomLoaded() {
+
+        document.querySelector('#calculate').addEventListener('click', calculateAndShow);
+
+        document.querySelector('#date1').addEventListener('input', calculateAndShow);
+
+        document.querySelector('#date2').addEventListener('input', calculateAndShow);
+    }
+})();
